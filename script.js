c85a3548949450294a1a4acee8a725943017d2e0
niksa <niksaopfor@gmail.com>
2022-04-06 16:23:40 +0200

    Working on project

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const navButton = document.querySelector('.nav__button');
+const background = document.querySelector('.background');
+const navLists = document.querySelector('.nav');
+
+navButton.addEventListener('click', function (e) {
+  navButton.classList.toggle('active');
+  background.classList.toggle('active');
+
+  navLists.classList.toggle('active');
+});
